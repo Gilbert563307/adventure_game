6f1b09b024f780bf3f25132baab167cf7155b9cb
Gilbert563307 <ssempijja759@gmail.com>
2024-10-15 23:42:25 +0200

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+
+import CollectRenderMap from './view/CollectRenderMap'
+
+function App() {
+  return (
+    <CollectRenderMap></CollectRenderMap>
+  )
+}
+
+export default App
---
+++ frontend/src/view/CollectRenderMap.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CollectRenderMap() {
+  return (
+    <div>CollectRenderMap</div>
+  )
+}
